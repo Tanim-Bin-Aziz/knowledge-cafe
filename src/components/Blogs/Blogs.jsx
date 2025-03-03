56c6b119566f8fd1f276bb1ae250ca4de6c21bea
Tanim-Bin-Aziz <tanimbinaziz145@gmail.com>
2025-03-03 22:00:29 +0600

    data fetch plus blogs data displayed

--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable no-unused-vars */
 import { useEffect } from "react";
 import { useState } from "react";
+import Blog from "../Blog/Blog";
 
 const Blogs = () => {
   const [blogs, setblogs] = useState([]);
@@ -14,6 +15,9 @@ const Blogs = () => {
   return (
     <div className="md:w-2/3">
       <h1 className="text-4xl">Blogs : {blogs.length}</h1>
+      {blogs.map((blog) => (
+        <Blog key={blog.id} blog={blog}></Blog>
+      ))}
     </div>
   );
 };
